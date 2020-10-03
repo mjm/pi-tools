20657c2fbd55b1db41d1b122f971411df4f6225c
Matt Moriarity <matt@mattmoriarity.com>
2020-10-02 18:06:28 -0600

    Add systemd unit file for detect-presence

--- tools/deploy.sh
+++ tools/deploy.sh
@@ -17,6 +17,6 @@ file "$BUILT_PATH"
 chmod u+w "$BUILT_PATH"
 scp "$BUILT_PATH" $PI_USER@$PI_HOST:$DEST_PATH
 
-if [[ "$COMMAND_NAME" = "detect-presence" ]]; then
-  ssh $PI_USER@$PI_HOST sudo setcap cap_net_raw+ep "$DEST_PATH"
+if [[ "$COMMAND_NAME" = "detect-presence-srv" ]]; then
+  scp detect-presence/cmd/detect-presence-srv/detect-presence.service $PI_USER@$PI_HOST:/deploy/pi-tools/detect-presence.service
 fi
