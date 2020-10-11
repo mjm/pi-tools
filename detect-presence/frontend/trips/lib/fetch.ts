bdbc3b436613821a9e7ac34e304e96987f8dfe3d
Matt Moriarity <matt@mattmoriarity.com>
2020-10-10 21:36:46 -0600

    Use the same origin for API requests in production

--- detect-presence/frontend/trips/lib/fetch.ts
+++ detect-presence/frontend/trips/lib/fetch.ts
@@ -1,22 +1,24 @@
 import {TripsServiceClient} from "com_github_mjm_pi_tools/detect-presence/proto/trips/trips_pb_service";
 import {ListTripsRequest} from "com_github_mjm_pi_tools/detect-presence/proto/trips/trips_pb";
 
-const client = new TripsServiceClient('http://localhost:2120')
+const serviceHost = process.env.NODE_ENV === "development" ? "http://localhost:2120" : window.location.href;
+
+const client = new TripsServiceClient(serviceHost);
 
 export async function fetcher(method: string, ...args: any[]): Promise<any> {
     switch (method) {
-        case 'ListTrips':
-            const req = new ListTripsRequest()
+        case "ListTrips":
+            const req = new ListTripsRequest();
             return new Promise((resolve, reject) => {
                 client.listTrips(req, (err, res) => {
                     if (err) {
-                        reject(err)
+                        reject(err);
                     } else {
-                        resolve(res.getTripsList())
+                        resolve(res.getTripsList());
                     }
-                })
-            })
+                });
+            });
         default:
-            throw new Error(`Unimplemented method to fetch: ${method}`)
+            throw new Error(`Unimplemented method to fetch: ${method}`);
     }
 }
--- detect-presence/frontend/trips/rollup.config.js
+++ detect-presence/frontend/trips/rollup.config.js
@@ -2,13 +2,16 @@ import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import replace from "@rollup/plugin-replace";
 
+console.log(process.env)
+const env = process.env.COMPILATION_MODE === 'opt' ? 'production' : 'development';
+
 export default {
     output: {
     },
     plugins: [
         replace({
             // TODO use bazel compilation mode
-            'process.env.NODE_ENV': JSON.stringify('production'),
+            'process.env.NODE_ENV': JSON.stringify(env),
         }),
         nodeResolve(),
         commonjs(),
