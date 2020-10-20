95cf2dc488f7c55a1daa7793d6e2841998e001bf
Matt Moriarity <matt@mattmoriarity.com>
2020-10-19 22:17:59 -0600

    Delete unused files and build target

--- tools/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Build a Bazel target on the Raspberry Pi
-
-TARGET="$1"
-
-# Workaround something broken in Bazel/rules_cc/rules_go's handling of these flags
-export BAZEL_LINKOPTS="-lstdc++:-lm:-latomic"
-bazel build -c opt "$TARGET"
--- tools/deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Build a tarball of all of the pi-tools and extract it on the Raspberry Pi
-
-PI_USER=pi
-PI_HOST=10.0.0.2
-
-ssh $PI_USER@$PI_HOST "cd /home/pi/pi-tools && git pull && ./tools/deploy-local.sh"
