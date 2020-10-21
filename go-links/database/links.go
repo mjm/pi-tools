2919e8b415028dd655c013223491e528528daf7f
Matt Moriarity <matt@mattmoriarity.com>
2020-10-20 23:45:03 -0600

    Add create link method to DB

--- go-links/database/links.go
+++ go-links/database/links.go
@@ -1,1 +1,43 @@
 package database
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	sq "github.com/Masterminds/squirrel"
+	"github.com/google/uuid"
+)
+
+type Link struct {
+	ID             string
+	ShortURL       string
+	DestinationURL string
+	Description    string
+	CreatedAt      time.Time
+	UpdatedAt      time.Time
+}
+
+type CreateLinkParams struct {
+	ShortURL       string
+	DestinationURL string
+	Description    string
+}
+
+func (c *Client) CreateLink(ctx context.Context, params CreateLinkParams) (*Link, error) {
+	id := uuid.New().String()
+	if _, err := sq.Insert("links").
+		Columns("id", "short_url", "destination_url", "description").
+		Values(id, params.ShortURL, params.DestinationURL, params.Description).
+		RunWith(c.db).
+		ExecContext(ctx); err != nil {
+		return nil, fmt.Errorf("creating link: %w", err)
+	}
+
+	return &Link{
+		ID:             id,
+		ShortURL:       params.ShortURL,
+		DestinationURL: params.DestinationURL,
+		Description:    params.Description,
+	}, nil
+}
