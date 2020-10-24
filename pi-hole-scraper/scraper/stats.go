77e0542619acae5f3c1406a3489fc23a1596eda8
Matt Moriarity <matt@mattmoriarity.com>
2020-10-24 12:08:40 -0600

    Delete some old things that have been superseded.

--- pi-hole-scraper/scraper/stats.go
+++
@@ -1,8 +0,0 @@
-package scraper
-
-type Stats struct {
-	AdsBlockedToday     int     `json:"ads_blocked_today"`
-	AdsPercentageToday  float64 `json:"ads_percentage_today"`
-	DomainsBeingBlocked int     `json:"domains_being_blocked"`
-	DNSQueriesToday     int     `json:"dns_queries_today"`
-}
