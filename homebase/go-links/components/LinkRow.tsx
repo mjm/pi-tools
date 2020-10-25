3cb657c083e273ea79d071031b90ddadc7e7cd9f
Matt Moriarity <matt@mattmoriarity.com>
2020-10-25 17:46:39 -0600

    Link to the detail page from the go links home

--- homebase/go-links/components/LinkRow.tsx
+++ homebase/go-links/components/LinkRow.tsx
@@ -1,18 +1,24 @@
 import React from "react";
+import {Link as RouterLink, useRouteMatch} from "react-router-dom";
 import {Link} from "com_github_mjm_pi_tools/go-links/proto/links/links_pb";
 import {destinationURL} from "com_github_mjm_pi_tools/homebase/go-links/lib/links_client";
 
 export function LinkRow({link}: { link: Link }) {
+    const {path} = useRouteMatch();
+
     return (
         <li className="border-t border-gray-200">
-            <a href={destinationURL(link.getShortUrl())}
-               target="_blank"
-               className="block hover:bg-gray-50 focus:outline-none focus:bg-gray-50 transition duration-150 ease-in-out">
+            <RouterLink to={`${path}/${link.getId()}`}
+                        className="block hover:bg-gray-50 focus:outline-none focus:bg-gray-50 transition duration-150 ease-in-out">
                 <div className="px-4 py-4 sm:px-6">
                     <div className="flex items-center justify-between">
-                        <div className="text-sm leading-5 font-medium text-indigo-600 truncate">
+                        <a href={destinationURL(link.getShortUrl())} target="_blank"
+                           className="block text-sm leading-5 font-medium text-indigo-600 truncate"
+                           onClick={e => {
+                               e.stopPropagation();
+                           }}>
                             go/{link.getShortUrl()}
-                        </div>
+                        </a>
                     </div>
                     <div className="mt-2">
                         <div className="flex items-center text-sm leading-5 text-gray-500">
@@ -20,7 +26,7 @@ export function LinkRow({link}: { link: Link }) {
                         </div>
                     </div>
                 </div>
-            </a>
+            </RouterLink>
         </li>
     );
 }
