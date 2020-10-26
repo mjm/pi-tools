8845ddcf848abbf5ce0f7aad6b20529cf6b4770d
Matt Moriarity <matt@mattmoriarity.com>
2020-10-26 00:11:42 -0600

    Add tracing to the HCI detector

--- detect-presence/detector/detector.go
+++ detect-presence/detector/detector.go
@@ -2,7 +2,13 @@ package detector
 
 import (
 	"context"
+
+	"go.opentelemetry.io/otel/api/global"
 )
+
+const instrumentationName = "github.com/mjm/pi-tools/detect-presence/detector"
+
+var tracer = global.Tracer(instrumentationName)
 
 type Detector interface {
 	IsHealthy(ctx context.Context) (bool, error)
--- detect-presence/detector/hci_detector.go
+++ detect-presence/detector/hci_detector.go
@@ -3,9 +3,12 @@ package detector
 import (
 	"context"
 	"io/ioutil"
-	"log"
 	"os/exec"
 	"strings"
+
+	"go.opentelemetry.io/otel/api/trace"
+	"go.opentelemetry.io/otel/codes"
+	"go.opentelemetry.io/otel/label"
 )
 
 type HCIDetector struct {
@@ -13,6 +16,10 @@ type HCIDetector struct {
 }
 
 func (d *HCIDetector) IsHealthy(ctx context.Context) (bool, error) {
+	ctx, span := tracer.Start(ctx, "HCIDetector.IsHealthy",
+		trace.WithAttributes(label.String("bluetooth.device.name", d.DeviceName)))
+	defer span.End()
+
 	cmd := exec.CommandContext(ctx, "/bin/hciconfig", d.DeviceName)
 	outPipe, err := cmd.StdoutPipe()
 	if err != nil {
@@ -24,6 +31,7 @@ func (d *HCIDetector) IsHealthy(ctx context.Context) (bool, error) {
 	}
 
 	if err := cmd.Start(); err != nil {
+		span.SetStatus(codes.Error, err.Error())
 		return false, err
 	}
 
@@ -38,22 +46,27 @@ func (d *HCIDetector) IsHealthy(ctx context.Context) (bool, error) {
 
 	if err := cmd.Wait(); err != nil {
 		if exitErr, ok := err.(*exec.ExitError); ok {
+			span.SetAttributes(label.Int("cmd.exit_code", exitErr.ExitCode()))
+
 			if exitErr.ExitCode() == 1 {
-				log.Printf("Detection failed, output: %s", errOutput)
+				span.SetStatus(codes.Error, string(errOutput))
 				return false, nil
 			}
 		}
+		span.SetStatus(codes.Error, err.Error())
 		return false, err
 	}
 
-	if strings.Contains(string(output), "UP RUNNING") {
-		return true, nil
-	}
-
-	return false, nil
+	healthy := strings.Contains(string(output), "UP RUNNING")
+	span.SetAttributes(label.Bool("bluetooth.healthy", healthy))
+	return healthy, nil
 }
 
 func (*HCIDetector) DetectDevice(ctx context.Context, addr string) (bool, error) {
+	ctx, span := tracer.Start(ctx, "HCIDetector.DetectDevice",
+		trace.WithAttributes(label.String("device.addr", addr)))
+	defer span.End()
+
 	cmd := exec.CommandContext(ctx, "/usr/bin/hcitool", "info", addr)
 	errPipe, err := cmd.StderrPipe()
 	if err != nil {
@@ -71,13 +84,18 @@ func (*HCIDetector) DetectDevice(ctx context.Context, addr string) (bool, error)
 
 	if err := cmd.Wait(); err != nil {
 		if exitErr, ok := err.(*exec.ExitError); ok {
+			span.SetAttributes(label.Int("cmd.exit_code", exitErr.ExitCode()))
+
 			if exitErr.ExitCode() == 1 {
-				log.Printf("Detection failed, output: %s", errOutput)
+				span.SetAttributes(label.Bool("device.present", false))
+				span.SetStatus(codes.Error, string(errOutput))
 				return false, nil
 			}
 		}
+		span.SetStatus(codes.Error, err.Error())
 		return false, err
 	}
 
+	span.SetAttributes(label.Bool("device.present", true))
 	return true, nil
 }
