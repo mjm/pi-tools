331494589020f8e6af37b0d37600714698476314
Matt Moriarity <matt@mattmoriarity.com>
2020-11-08 15:15:15 -0700

    Add deploy-k8s.sh script to pull artifact from GitHub actions

---
+++ tools/deploy-k8s.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -o pipefail
+set -xe
+
+org=mjm
+repo=pi-tools
+branch=main
+workflow=k8s.yaml
+
+artifacts_url=$(curl -s "https://api.github.com/repos/$org/$repo/actions/workflows/$workflow/runs?branch=$branch&status=success&per_page=1" | jq -r '.workflow_runs[0].artifacts_url')
+archive_url=$(curl -s "$artifacts_url" | jq -r '.artifacts[] | select(.name == "all_k8s") | .archive_download_url')
+
+echo "Downloading archive from ${archive_url}" >&2
+
+creds=$(cat "$HOME/.github_auth.txt")
+
+cd "$(mktemp -d)" || exit 1
+curl -sL "$archive_url" -u "$creds" -o archive.zip
+unzip archive.zip
+
+/snap/bin/microk8s kubectl apply -f all_k8s.yaml
+
+rm all_k8s.yaml archive.zip
