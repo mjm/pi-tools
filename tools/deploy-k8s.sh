4d188ede8710e02c9a53583b6598d8bf450d1ee0
Matt Moriarity <matt@mattmoriarity.com>
2020-11-08 15:37:09 -0700

    Split CRDs into their own file so we don't have to apply them every time

--- tools/deploy-k8s.sh
+++ tools/deploy-k8s.sh
@@ -8,6 +8,8 @@ repo=pi-tools
 branch=main
 workflow=k8s.yaml
 
+file_to_apply="$1"
+
 artifacts_url=$(curl -s "https://api.github.com/repos/$org/$repo/actions/workflows/$workflow/runs?branch=$branch&status=success&per_page=1" | jq -r '.workflow_runs[0].artifacts_url')
 archive_url=$(curl -s "$artifacts_url" | jq -r '.artifacts[] | select(.name == "all_k8s") | .archive_download_url')
 
@@ -19,6 +21,6 @@ cd "$(mktemp -d)" || exit 1
 curl -sL "$archive_url" -u "$creds" -o archive.zip
 unzip archive.zip
 
-/snap/bin/microk8s kubectl apply -f all_k8s.yaml
+/snap/bin/microk8s kubectl apply -f "$file_to_apply.yaml"
 
-rm all_k8s.yaml archive.zip
+rm *.yaml archive.zip
