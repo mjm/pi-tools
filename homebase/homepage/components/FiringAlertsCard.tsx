0bc6e73662f5dcd4cb0b02a58f44afcc5947037f
Matt Moriarity <matt@mattmoriarity.com>
2020-11-17 21:34:49 -0700

    Use proper icon for firing alerts card

--- homebase/homepage/components/FiringAlertsCard.tsx
+++ homebase/homepage/components/FiringAlertsCard.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import {Link} from "react-router-dom";
 import useSWR from "swr";
 import {PrometheusAlert} from "com_github_mjm_pi_tools/homebase/homepage/lib/prometheus";
 import {fetcher, LIST_FIRING_ALERTS} from "com_github_mjm_pi_tools/homebase/homepage/lib/fetch";
@@ -25,7 +24,7 @@ export function FiringAlertsCard() {
                                 <svg className="h-6 w-6 text-cool-gray-400" xmlns="http://www.w3.org/2000/svg"
                                      fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
-                                          d="M9 20l-5.447-2.724A1 1 0 013 16.382V5.618a1 1 0 011.447-.894L9 7m0 13l6-3m-6 3V7m6 10l4.553 2.276A1 1 0 0021 18.382V7.618a1 1 0 00-.553-.894L15 4m0 13V4m0 0L9 7"/>
+                                          d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"/>
                                 </svg>
                             </div>
                             <div className="ml-5 w-0 flex-1">
@@ -53,7 +52,7 @@ export function FiringAlertsCard() {
                     <div className="bg-cool-gray-50 px-5 py-3">
                         <div className="text-sm leading-5">
                             <a href="https://alertmanager.homelab/"
-                                  className="font-medium text-indigo-600 hover:text-indigo-900 transition ease-in-out duration-150">
+                               className="font-medium text-indigo-600 hover:text-indigo-900 transition ease-in-out duration-150">
                                 View active alerts
                             </a>
                         </div>
