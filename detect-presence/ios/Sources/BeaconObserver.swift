f93f9dfdd6d8fc079b838b64f72e430594f60cb6
Matt Moriarity <matt@mattmoriarity.com>
2020-11-25 14:15:46 -0700

    Add trip recorder to keep track of trips locally on iPhone

--- detect-presence/ios/Sources/BeaconObserver.swift
+++ detect-presence/ios/Sources/BeaconObserver.swift
@@ -1,51 +1,20 @@
 import CoreLocation
-import detect_presence_proto_trips_trips_proto
-import detect_presence_proto_trips_trips_swift_proto_grpc_client
 
 private let beaconIdentifier = "home-beacons"
 
-// probably a better way to do this but it works!
-private let homelabCA = """
------BEGIN CERTIFICATE-----
-MIIDVjCCAj6gAwIBAgIBATANBgkqhkiG9w0BAQsFADBQMRowGAYDVQQDDBFNYXR0
-J3MgSG9tZWxhYiBDQTELMAkGA1UEBhMCVVMxJTAjBgkqhkiG9w0BCQEWFm1hdHRA
-bWF0dG1vcmlhcml0eS5jb20wHhcNMjAxMTA4MTY0OTA4WhcNMjExMTA4MTY0OTA4
-WjBQMRowGAYDVQQDDBFNYXR0J3MgSG9tZWxhYiBDQTELMAkGA1UEBhMCVVMxJTAj
-BgkqhkiG9w0BCQEWFm1hdHRAbWF0dG1vcmlhcml0eS5jb20wggEiMA0GCSqGSIb3
-DQEBAQUAA4IBDwAwggEKAoIBAQDRl2HGjFNsNVVs/l3tRWPd4C1UonuBFgPYNnkv
-ZR7mxovM0lCgVVv9RSiqEK6E5gUO3US383G09VO04fITwGbpLRaYDMqfRfFkayWb
-Hqtxu/oNeNlw8upL/dByvICSlLcSlcpWWqLkrYVTCU2LripHMv2Kqsbcyps8seM/
-F9Ie5Mj3jBn01FJ9veq+a9kkW1CzcpnzBXWgqmczXlJ69iLTJq8qQ7O64RVMel74
-kBaxvUT6exkgjFcRjvXrecRxNEOEiKIhIruDxtJIqgx5sZDVdOoPBYUKdpIW2bhF
-9ax720UV1phcAul8gBMevus0S0y5o38yLjcSs4UlDVFtERy1AgMBAAGjOzA5MA8G
-A1UdEwEB/wQFMAMBAf8wDgYDVR0PAQH/BAQDAgKkMBYGA1UdJQEB/wQMMAoGCCsG
-AQUFBwMBMA0GCSqGSIb3DQEBCwUAA4IBAQDOw07kOn9BI0OMwvHiua5H0vLPM58s
-7jCfUZRWmXHwStq948QslNWVzbIbn7xj7EDUIIciu3ubAWMx/hY/6diq640JzTN1
-50U6ntoSYQcTmbAGBrM6VhXwNJSLmAB9LdVwh6a3tXVqgKZgqdYFHmbMxg/NfGln
-WjzK7/ytsObOLhq9xGwhJdmKlGwUuQydbRtl8hqJ63toJ1xingH+/nhaiJRA+3Uu
-J8N0sUUTv25QS7WKKSJWRcgX3GmEP8IJtRICcz/buPCGoreLD3Wcl171iDYIEYHq
-9+mDsO5/lPY1EGk09S0hqgq65mNhZIYq9jpPlQ2ZkzP+2ZXgNKBi1cNQ
------END CERTIFICATE-----
-"""
-
 class BeaconObserver: NSObject, CLLocationManagerDelegate, ObservableObject {
-    var locationManager: CLLocationManager
-    var tripsClient: TripsServiceService
+    let locationManager: CLLocationManager
+    let tripRecorder: TripRecorder
 
     @Published private(set) var status: CLRegionState = .unknown
     @Published private(set) var statusChangedTime: Date?
 
     init(locationManager: CLLocationManager = CLLocationManager()) {
         self.locationManager = locationManager
-        self.tripsClient = TripsServiceServiceClient(address: "detect-presence-grpc.homelab", certificates: homelabCA)
+        self.tripRecorder = TripRecorder()
         super.init()
 
         self.locationManager.delegate = self
-
-        try! self.tripsClient.listTrips(ListTripsRequest()) { response, result in
-            NSLog("Response: \(response)")
-            NSLog("Result: \(result)")
-        }
     }
 
     func startObserving() {
@@ -68,6 +37,8 @@ class BeaconObserver: NSObject, CLLocationManagerDelegate, ObservableObject {
         case .authorizedWhenInUse:
             manager.requestAlwaysAuthorization()
             startObserving()
+        case .authorizedAlways:
+            startObserving()
         default:
             break
         }
@@ -89,10 +60,12 @@ class BeaconObserver: NSObject, CLLocationManagerDelegate, ObservableObject {
 
     func locationManager(_ manager: CLLocationManager, didEnterRegion region: CLRegion) {
         NSLog("Entered region: \(region)")
+        tripRecorder.endTrip()
     }
 
     func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
         NSLog("Exited region: \(region)")
+        tripRecorder.beginTrip()
     }
 
     func locationManager(_ manager: CLLocationManager, monitoringDidFailFor region: CLRegion?, withError error: Error) {
---
+++ detect-presence/ios/Sources/TripRecorder.swift
@@ -0,0 +1,64 @@
+import Foundation
+import detect_presence_proto_trips_trips_proto
+import detect_presence_proto_trips_trips_swift_proto_grpc_client
+
+private let dateFormatter = ISO8601DateFormatter()
+
+class TripRecorder {
+    private let client: TripsServiceService
+
+    private var currentTrip: Trip?
+
+    init() {
+        client = TripsServiceServiceClient(address: "detect-presence-grpc.homelab", certificates: homelabCA)
+    }
+
+    func beginTrip() {
+        guard currentTrip == nil else {
+            NSLog("Already have a current trip, so not beginning a new one.")
+            return
+        }
+
+        currentTrip = .with {
+            $0.id = UUID().uuidString
+            $0.leftAt = dateFormatter.string(from: Date())
+        }
+        NSLog("starting trip \(currentTrip!)")
+    }
+
+    func endTrip() {
+        guard var trip = currentTrip else {
+            NSLog("No trip in progress, nothing to end.")
+            return
+        }
+
+        trip.returnedAt = dateFormatter.string(from: Date())
+        NSLog("ending trip \(trip)")
+
+        // TODO make gRPC request to record the trip
+    }
+}
+
+// probably a better way to do this but it works!
+private let homelabCA = """
+-----BEGIN CERTIFICATE-----
+MIIDVjCCAj6gAwIBAgIBATANBgkqhkiG9w0BAQsFADBQMRowGAYDVQQDDBFNYXR0
+J3MgSG9tZWxhYiBDQTELMAkGA1UEBhMCVVMxJTAjBgkqhkiG9w0BCQEWFm1hdHRA
+bWF0dG1vcmlhcml0eS5jb20wHhcNMjAxMTA4MTY0OTA4WhcNMjExMTA4MTY0OTA4
+WjBQMRowGAYDVQQDDBFNYXR0J3MgSG9tZWxhYiBDQTELMAkGA1UEBhMCVVMxJTAj
+BgkqhkiG9w0BCQEWFm1hdHRAbWF0dG1vcmlhcml0eS5jb20wggEiMA0GCSqGSIb3
+DQEBAQUAA4IBDwAwggEKAoIBAQDRl2HGjFNsNVVs/l3tRWPd4C1UonuBFgPYNnkv
+ZR7mxovM0lCgVVv9RSiqEK6E5gUO3US383G09VO04fITwGbpLRaYDMqfRfFkayWb
+Hqtxu/oNeNlw8upL/dByvICSlLcSlcpWWqLkrYVTCU2LripHMv2Kqsbcyps8seM/
+F9Ie5Mj3jBn01FJ9veq+a9kkW1CzcpnzBXWgqmczXlJ69iLTJq8qQ7O64RVMel74
+kBaxvUT6exkgjFcRjvXrecRxNEOEiKIhIruDxtJIqgx5sZDVdOoPBYUKdpIW2bhF
+9ax720UV1phcAul8gBMevus0S0y5o38yLjcSs4UlDVFtERy1AgMBAAGjOzA5MA8G
+A1UdEwEB/wQFMAMBAf8wDgYDVR0PAQH/BAQDAgKkMBYGA1UdJQEB/wQMMAoGCCsG
+AQUFBwMBMA0GCSqGSIb3DQEBCwUAA4IBAQDOw07kOn9BI0OMwvHiua5H0vLPM58s
+7jCfUZRWmXHwStq948QslNWVzbIbn7xj7EDUIIciu3ubAWMx/hY/6diq640JzTN1
+50U6ntoSYQcTmbAGBrM6VhXwNJSLmAB9LdVwh6a3tXVqgKZgqdYFHmbMxg/NfGln
+WjzK7/ytsObOLhq9xGwhJdmKlGwUuQydbRtl8hqJ63toJ1xingH+/nhaiJRA+3Uu
+J8N0sUUTv25QS7WKKSJWRcgX3GmEP8IJtRICcz/buPCGoreLD3Wcl171iDYIEYHq
+9+mDsO5/lPY1EGk09S0hqgq65mNhZIYq9jpPlQ2ZkzP+2ZXgNKBi1cNQ
+-----END CERTIFICATE-----
+"""
