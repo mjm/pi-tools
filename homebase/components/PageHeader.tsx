d534e450f62ade0843321f7440ecc1535d3bf14d
Matt Moriarity <matt@mattmoriarity.com>
2020-11-29 11:55:26 -0700

    Add button to download presence app on trips page

--- homebase/components/PageHeader.tsx
+++ homebase/components/PageHeader.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 
-export function PageHeader({children}: { children: React.ReactNode }) {
+export function PageHeader({children, buttons}: {
+    children: React.ReactNode;
+    buttons?: React.ReactNode;
+}) {
     return (
         <header className="bg-white shadow">
             <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                <h1 className="text-3xl font-bold leading-tight text-gray-900">
-                    {children}
-                </h1>
+                <div className="md:flex md:items-center md:justify-between">
+                    <div className="flex-1 min-w-0">
+                        <h1 className="text-2xl font-bold leading-tight text-gray-900 sm:text-3xl sm:truncate">
+                            {children}
+                        </h1>
+                    </div>
+                    {buttons ? (
+                        <div className="mt-4 flex md:mt-0 md:ml-4">
+                            {buttons}
+                        </div>
+                    ) : null}
+                </div>
             </div>
         </header>
     );
--- homebase/trips/components/TripsPage.tsx
+++ homebase/trips/components/TripsPage.tsx
@@ -21,7 +21,15 @@ export function TripsPage() {
                 <title>Your Trips</title>
             </Helmet>
 
-            <PageHeader>
+            <PageHeader buttons={<>
+                <a href="/download_app"
+                   className="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                    <svg className="-ml-1 mr-2 h-5 w-5 text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4" />
+                    </svg>
+                    Download app
+                </a>
+            </>}>
                 Your trips
             </PageHeader>
             <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
@@ -38,7 +46,8 @@ export function TripsPage() {
                                     <table className="min-w-full divide-y divide-gray-200">
                                         <thead>
                                         <tr>
-                                            <td colSpan={4} className="px-6 py-3 bg-gray-50 text-xs leading-4 text-gray-500 border-b border-gray-200">
+                                            <td colSpan={4}
+                                                className="px-6 py-3 bg-gray-50 text-xs leading-4 text-gray-500 border-b border-gray-200">
                                                 <TagFilters/>
                                             </td>
                                         </tr>
