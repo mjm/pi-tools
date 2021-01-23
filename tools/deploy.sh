c0bb38fbb208756e2cdc53046d37a478366a5bbc
Matt Moriarity <matt@mattmoriarity.com>
2021-01-23 16:13:34 -0700

    Create shell script to deploy jobs on-demand

---
+++ tools/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+
+function join { local IFS="$1"; shift; echo "$*"; }
+
+cd "$(git rev-parse --show-toplevel)"
+
+out_path="$(mktemp -d)"
+function finish {
+  rm -rf "$out_path"
+}
+trap finish EXIT
+
+jobs_pattern=".*/($(join '|' "$@"))\\.nomad"
+
+find -E "$PWD/jobs" -regex "$jobs_pattern" -exec \
+  bazel run //deploy/cmd/job-resolver -- -root "$PWD/jobs" -out "$out_path" '{}' +
+
+bazel run //deploy/cmd/deploy -- $out_path/*.json
