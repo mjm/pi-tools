187a3704fdfa5b4d1dfb0c4cf42173bcfb5a8d20
Matt Moriarity <matt@mattmoriarity.com>
2021-04-06 20:47:08 -0600

    Use correct icons and styling based on event level

--- homebase/components/deploys/DeploymentEvent.tsx
+++ homebase/components/deploys/DeploymentEvent.tsx
@@ -33,6 +33,37 @@ export default function DeploymentEvent({deploy, event, isLast}: {
     const deployStartTime = parseISO(deployData.startedAt);
     const secondsSinceStart = getUnixTime(eventTime) - getUnixTime(deployStartTime);
 
+    let iconStyle = "";
+    let iconContent = null;
+    let summaryStyle = "";
+    switch (data.level) {
+        case "ERROR":
+            iconStyle = "bg-red-600";
+            iconContent =
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                     stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12"/>
+                </svg>;
+            summaryStyle = "font-bold";
+            break;
+        case "WARNING":
+            iconStyle = "bg-yellow-500";
+            iconContent =
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                     stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2}
+                          d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"/>
+                </svg>;
+            break;
+        default:
+            iconStyle = "bg-gray-400";
+            iconContent =
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24"
+                     stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7"/>
+                </svg>;
+    }
+
     return (
         <li>
             <div className="relative pb-8">
@@ -40,15 +71,14 @@ export default function DeploymentEvent({deploy, event, isLast}: {
                                   aria-hidden="true"/>}
                 <div className="relative flex space-x-3">
                     <div>
-            <span className="h-8 w-8 rounded-full bg-gray-400 flex items-center justify-center ring-8 ring-white">
-<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7"/>
-</svg>
+            <span
+                className={`h-8 w-8 rounded-full text-white flex items-center justify-center ring-8 ring-white ${iconStyle}`}>
+                {iconContent}
             </span>
                     </div>
                     <div className="min-w-0 flex-1 pt-1.5 flex justify-between space-x-4">
                         <div>
-                            <p className="text-sm text-gray-700">{data.summary}</p>
+                            <p className={`text-sm text-gray-700 ${summaryStyle}`}>{data.summary}</p>
                         </div>
                         <div className="text-right text-sm whitespace-nowrap text-gray-500">
                             <time dateTime={data.timestamp}>{convertToDuration(secondsSinceStart)}</time>
