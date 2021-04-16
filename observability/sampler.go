6bd1e4e81903b6db37e2d366e82915b2a4a4d1f5
Matt Moriarity <matt@mattmoriarity.com>
2021-04-16 09:06:58 -0600

    Sample successful auth requests in vault-proxy

--- observability/sampler.go
+++ observability/sampler.go
@@ -7,6 +7,7 @@ import (
 type customSampler struct {
 	defaultSampler     trace.Sampler
 	healthcheckSampler trace.Sampler
+	authRequestSampler trace.Sampler
 }
 
 func (s customSampler) ShouldSample(parameters trace.SamplingParameters) trace.SamplingResult {
@@ -15,6 +16,16 @@ func (s customSampler) ShouldSample(parameters trace.SamplingParameters) trace.S
 			if string(kv.Key) == "http.target" {
 				if kv.Value.AsString() == "/healthz" {
 					return s.healthcheckSampler.ShouldSample(parameters)
+				}
+				if kv.Value.AsString() == "/auth" {
+					for _, kv2 := range parameters.Attributes {
+						if string(kv2.Key) == "http.status_code" {
+							if kv.Value.AsInt64() == 200 {
+								return s.authRequestSampler.ShouldSample(parameters)
+							}
+							break
+						}
+					}
 				}
 				break
 			}
@@ -32,5 +43,6 @@ func DefaultSampler() trace.Sampler {
 	return trace.ParentBased(customSampler{
 		defaultSampler:     trace.AlwaysSample(),
 		healthcheckSampler: trace.TraceIDRatioBased(0.1),
+		authRequestSampler: trace.TraceIDRatioBased(0.1),
 	})
 }
