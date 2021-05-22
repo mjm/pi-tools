ba7a68bbe9806d7bcfd5755d80bc1595a5c195f2
Matt Moriarity <matt@mattmoriarity.com>
2021-05-22 17:05:46 -0600

    Fix prune script when there are no archives to prune

--- jobs/backup-tarsnap/prune.sh
+++ jobs/backup-tarsnap/prune.sh
@@ -4,4 +4,4 @@ set -e
 
 tarsnap --keyfile {{ env "NOMAD_SECRETS_DIR" }}/tarsnap.key --cachedir /var/lib/tarsnap/cache --no-default-config --list-archives \
   | prunef --keep-daily 7 --keep-weekly 4 --keep-monthly 6 daily-backup-%Y-%m-%d_%H-%M-%S \
-  | xargs -n1 tarsnap --keyfile {{ env "NOMAD_SECRETS_DIR" }}/tarsnap.key --cachedir /var/lib/tarsnap/cache --no-default-config -v -d -f
+  | xargs -r -n1 tarsnap --keyfile {{ env "NOMAD_SECRETS_DIR" }}/tarsnap.key --cachedir /var/lib/tarsnap/cache --no-default-config -v -d -f
