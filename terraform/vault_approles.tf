58c25ed75baae5081ed49e598809caeb205aaf40
Matt Moriarity <matt@mattmoriarity.com>
2022-11-14 16:48:40 -0700

    Remove IP requirements for app roles

--- terraform/vault_approles.tf
+++ terraform/vault_approles.tf
@@ -1,39 +1,34 @@
 resource "vault_approle_auth_backend_role" "homelab" {
   backend = vault_auth_backend.approle.id
 
-  role_name = "homelab"
-  #secret_id_bound_cidrs = ["10.0.2.114/32"]
+  role_name      = "homelab"
   token_policies = ["homelab"]
 }
 
 resource "vault_approle_auth_backend_role" "paperless" {
   backend = vault_auth_backend.approle.id
 
-  role_name             = "paperless"
-  secret_id_bound_cidrs = ["10.0.2.110/32"]
-  token_policies        = ["paperless"]
+  role_name      = "paperless"
+  token_policies = ["paperless"]
 }
 
 resource "vault_approle_auth_backend_role" "phabricator" {
   backend = vault_auth_backend.approle.id
 
-  role_name             = "phabricator"
-  secret_id_bound_cidrs = ["10.0.2.111/32"]
-  token_policies        = ["phabricator"]
+  role_name      = "phabricator"
+  token_policies = ["phabricator"]
 }
 
 resource "vault_approle_auth_backend_role" "teamcity" {
   backend = vault_auth_backend.approle.id
 
-  role_name             = "teamcity"
-  secret_id_bound_cidrs = ["10.0.2.113/32"]
-  token_policies        = ["teamcity"]
+  role_name      = "teamcity"
+  token_policies = ["teamcity"]
 }
 
 resource "vault_approle_auth_backend_role" "prometheus" {
   backend = vault_auth_backend.approle.id
 
-  role_name             = "prometheus"
-  secret_id_bound_cidrs = ["10.0.2.104/32"]
-  token_policies        = ["prometheus", "alertmanager"]
+  role_name      = "prometheus"
+  token_policies = ["prometheus", "alertmanager"]
 }
